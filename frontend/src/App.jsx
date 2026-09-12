88ec1ddb40e88db8a0d34ab19953246fd50740b9
Maksim Kalinski <makskrik07@gmail.com>
2026-09-12 12:50:49 +0300

    Добавил основу кинотеатра: React, PHP и Docker

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Button, Container, Title, Text } from '@mantine/core'
+
+function App() {
+  return (
+    <Container mt="xl">
+      <Title>Hello World</Title>
+
+      <Text mt="md">
+        Kinoshka frontend works
+      </Text>
+
+      <Button mt="md">
+        Mantine works
+      </Button>
+    </Container>
+  )
+}
+
+export default App
